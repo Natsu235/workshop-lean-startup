4d110e1b91324b63fcbbb8e196ddcb37975d57ca
Natsu235 <27864198+Natsu235@users.noreply.github.com>
2021-02-11 11:50:46 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/cosmo/bootstrap.min.css" integrity="sha384-5QFXyVb+lrCzdN228VS3HmzpiE7ZVwLQtkt+0d9W43LQMzz4HBnnqvVxKg6O+04d" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/styles.css">
+
+    <title>Mon Site - Landing Page</title>
+</head>
+
+<body>
+    <header>
+        <div class="jumbotron">
+            <div class="container">
+                <div class="logo pb-5">
+                    <h1>LOGO</h1>
+                    <hr class="my-4">
+                </div>
+                <div class="catchphrase pb-5">
+                    <h1 class="display-4">L'APPLICATION QUI<br>VOUS PROTEGE !</h1>
+                    <br>
+                    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc erat tellus, viverra vel eros nec, sodales luctus ligula.<br>Morbi in laoreet quam. Suspendisse eu dictum nibh. Vivamus ac tempor orci, sed dignissim elit. Suspendisse id auctor ex.</p>
+                </div>
+                <div class="btn-list">
+                    <img class="mr-5" src="assets/img/google-play.svg" alt="Google Play Store" height="80px" width="269px" />
+                    <img src="assets/img/app-store.svg" alt="Apple App Store" height="80px" width="240px" />
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
