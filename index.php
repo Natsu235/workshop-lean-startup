a733894a0dded66d5dabd77f1da7709a33556d00
Natsu235 <27864198+Natsu235@users.noreply.github.com>
2021-02-11 14:07:10 +0100

    Finished Template Overall

--- index.php
+++ index.php
@@ -5,13 +5,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/cosmo/bootstrap.min.css" integrity="sha384-5QFXyVb+lrCzdN228VS3HmzpiE7ZVwLQtkt+0d9W43LQMzz4HBnnqvVxKg6O+04d" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="assets/css/styles.css">
 
     <title>Mon Site - Landing Page</title>
 </head>
 
 <body>
-    <header>
+    <header id="header">
         <div class="jumbotron">
             <div class="container">
                 <div class="logo pb-5">
@@ -24,10 +25,87 @@
                     <p class="lead w-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc erat tellus, viverra vel eros nec, sodales luctus ligula. Morbi in laoreet quam. Suspendisse eu dictum nibh. Vivamus ac tempor orci, sed dignissim elit. Suspendisse id auctor ex.</p>
                 </div>
                 <div class="btn-list pb-5">
-                    <img class="mr-5" src="assets/img/google-play.svg" alt="Google Play Store" height="80px" width="269px" />
-                    <img src="assets/img/app-store.svg" alt="Apple App Store" height="80px" width="240px" />
+                    <a href="#">
+                        <img class="play-store mr-5" src="assets/img/google-play.svg" alt="Google Play Store" width="269px" height="80px" />
+                    </a>
+                    <a href="#">
+                        <img class="app-store" src="assets/img/app-store.svg" alt="Apple App Store" width="240px" height="80px" />
+                    </a>
                 </div>
             </div>
         </div>
     </header>
+
+    <div class="spacer"></div>
+
+    <div id="preview" class="content">
+        <div class="container">
+            <div class="category center">
+                <iframe src="https://www.youtube.com/watch?v=Ek6AIVcYFh4" width="680" height="420"></iframe> 
+            </div>
+        </div>
+    </div>
+
+    <div class="container py-5">
+        <hr class="my-4">
+    </div>
+
+    <div id="logos">
+        <div class="container">
+            <div class="center">
+                <h1 class="my-5">Lorem ipsum dolor sit amet,<br>consectetur adipiscing elit.</h1><br>
+                <div class="row mt-3">
+                    <div class="col-sm">
+                        <img class="logo1" src="assets/img/logo1.svg" alt="Logo 1" width="100" height="100" />
+                        <h2>SIGNALER</h2><br>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                    </div>
+                    <div class="col-sm">
+                        <img class="logo2" src="assets/img/logo2.svg" alt="Logo 2" width="100" height="100" />
+                        <h2>AIDER</h2><br>
+                        <p>Morbi in laoreet quam.</p>
+                    </div>
+                    <div class="col-sm">
+                        <img class="logo3" src="assets/img/logo3.svg" alt="Logo 3" width="100" height="100" />
+                        <h2>S'INFORMER</h2><br>
+                        <p>Consectetur adipiscing elit. Nunc erat tellus, viverra vel.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container py-5">
+        <hr class="my-4">
+    </div>
+
+    <div id="download">
+        <div class="center btn-list my-5">
+            <a href="#">
+                <img class="play-store-download mr-5" src="assets/img/google-play.svg" alt="Google Play Store" width="269px" height="80px" />
+            </a>
+            <a href="#">
+                <img class="app-store-download" src="assets/img/app-store.svg" alt="Apple App Store" width="240px" height="80px" />
+            </a>
+        </div>
+    </div>
+
+    <div class="spacer"></div>
+
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <!-- Popper.js -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.6.0/umd/popper.min.js" integrity="sha512-BmM0/BQlqh02wuK5Gz9yrbe7VyIVwOzD1o40yi1IsTjriX/NGF37NyXHfmFzIlMmoSIBXgqDiG1VNU6kB5dBbA==" crossorigin="anonymous"></script>
+    <!-- Bootstrap -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </body>
+
+<footer id="footer">
+    <div class="footer-content">
+        <div class="container">
+            <p>Conditions générales d’utilisation<br>
+               Mentions légales<br>
+               F.A.Q.</p>
+        </div>
+    </div>
+</footer>
