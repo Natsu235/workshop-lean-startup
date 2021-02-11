75cca09ac34ad7158988ebe7a4d3d8add2eb3381
Natsu235 <27864198+Natsu235@users.noreply.github.com>
2021-02-11 16:47:59 +0100

    Typo Fix

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/styles.css">
 
-    <title>Mon Site - Landing Page</title>
+    <title>BeSafe - L'application qui vous protège</title>
 </head>
 
 <body>
